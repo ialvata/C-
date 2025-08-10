591ec9e9a383fc22f3cfcbf457533ea02e9c452e
ialvata <ivo.a.v.tavares@gmail.com>
2025-08-10 19:59:24 +0100

    cleaned many folders

--- Professional_C++/hello.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-int main()
-{
-std::cout << "Hello, World!" << std::endl;
-return 0;
-}
-
-// to compile this file: g++ -std=c++20 -o hello hello.cpp
-// to run the compiled code: ./hello
